9af5132c7110e47151febf364e1bd726b8659478
chpwang <chpengwang@gmail.com>
2016-09-21 04:29:48 +0800

    refactor course controller testing using RSpec context

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -72,26 +72,32 @@ RSpec.describe CoursesController, type: :controller do
 
   describe "POST create" do
 
-    it "doesn't create a record when course doesn't have a title" do
-      expect{ post :create, params: { course: {description: "bar"} } }.to change{ Course.count }.by(0)
+    context "when course doesn't have a title" do
+      it "doesn't create a record" do
+        expect{ post :create, params: { course: {description: "bar"} } }.to change{ Course.count }.by(0)
+      end
+
+      it "render new template" do
+        post :create, params: {course: {description: "bar"}}
+        expect(response).to render_template("new")
+      end
     end
 
-    it "render new template record when course has a title" do
-      post :create, params: {course: {description: "bar"}}
-      expect(response).to render_template("new")
-    end
-
-    it "create a new course record when course has a title" do
-      course = FactoryGirl.build(:course)
-      expect{ post :create, params: {course: FactoryGirl.attributes_for(:course)} }.to change{ Course.count }.by(1)
-    end
+    
+    context "when course has a title" do
+      it "create a new course record" do
+        course = FactoryGirl.build(:course)
+        expect{ post :create, params: {course: FactoryGirl.attributes_for(:course)} }.to change{ Course.count }.by(1)
+      end
 
 
-    it "redirect to courses_path when course has a title" do
-      course = FactoryGirl.build(:course)
-      post :create, params: { course: FactoryGirl.attributes_for(:course) }
-      expect(response).to redirect_to courses_path
+      it "redirect to courses_path" do
+        course = FactoryGirl.build(:course)
+        post :create, params: { course: FactoryGirl.attributes_for(:course) }
+        expect(response).to redirect_to courses_path
+      end
     end
+    
   end
 
 end
