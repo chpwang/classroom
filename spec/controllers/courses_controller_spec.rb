a723cf0378cd276e8525c35cc048605ca7def375
chpwang <chpengwang@gmail.com>
2016-09-20 22:27:04 +0800

    perform rule - one assertion per test

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe CoursesController, type: :controller do
 
   describe "GET index" do
-    it "assigns @courses and render template" do
+    it "assigns @courses" do
 
       course1 = FactoryGirl.create(:course)
       course2 = FactoryGirl.create(:course)
@@ -11,8 +11,18 @@ RSpec.describe CoursesController, type: :controller do
       get :index
 
       expect(assigns[:courses]).to eq([course1, course2])
+      
+    end
+
+    it "render template" do
+      course1 = FactoryGirl.create(:course)
+      course2 = FactoryGirl.create(:course)
+
+      get :index
+      
       expect(response).to render_template("index")
     end
+
   end
 
 end
